2bebcd36614ebc2ac38238d0f9d8831a46375fff
Zia Zek <ziazek@gmail.com>
2017-03-02 15:18:33 +0800

    finished 11 and 12

--- lib/koans/05_atoms.ex
+++ lib/koans/05_atoms.ex
@@ -5,27 +5,27 @@ defmodule Atoms do
 
   koan "Atoms are sort of like strings" do
     adam = :human
-    assert adam == ___
+    assert adam == :human
   end
 
   koan "Strings can be converted to atoms, and vice versa" do
-    assert String.to_atom("atomized") == ___
-    assert Atom.to_string(:stringified) == ___
+    assert String.to_atom("atomized") == :atomized
+    assert Atom.to_string(:stringified) == "stringified"
   end
 
   koan "It is surprising to find out that booleans are atoms" do
-    assert is_atom(true) == ___
-    assert is_atom(false) == ___
-    assert :true == ___
-    assert :false == ___
+    assert is_atom(true) == true
+    assert is_atom(false) == true
+    assert :true == true
+    assert :false == false
   end
 
   koan "Modules are also atoms" do
-    assert is_atom(String) == ___
+    assert is_atom(String) == true
   end
 
   koan "Functions can be called on the atom too" do
     assert :"Elixir.String" == String
-    assert :"Elixir.String".upcase("hello") == ___
+    assert :"Elixir.String".upcase("hello") == "HELLO"
   end
 end
